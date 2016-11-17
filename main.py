3bbc1c80c901398101f5fad46d584debfb490978
Tanveer Ahmed <A-tanveer@users.noreply.github.com>
2016-11-17 15:11:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
-# BHORER_KAGOJ.
-
 import os
-import ngrammethod
-import getdate
+import NgramMethod
+import GetDate
 
 out = open('output data.txt', 'w', encoding='utf8')
 
 corpusDir = os.getcwd() + "\corpus"
 
 for filename in os.listdir(corpusDir):
-
-    # out.write(filename + '\n')
-
-    if filename.startswith('BHO') or filename.startswith('JUG') or filename.startswith('KAL'):
-        dateval = 1
-    elif filename.startswith('BD_'):
-        dateval = 2
-    elif filename.startswith('PROTHOM_ALO_ARCHIEVE.tx'):
-        dateval = 3
-    # elif filename.startswith('PROTHOM_ALO.tx'):
-        # dateval = 1
 
     x = 0
     text = ""
@@ -41,14 +28,19 @@ for filename in os.listdir(corpusDir):
                 temp.write(text)
                 temp.close()
 
-                output = ngrammethod.text_processor()
+                output = NgramMethod.text_processor()
                 if output > 0:
-                    if dateval == 1:
-                        date = getdate.get_date()
-                    elif dateval == 2:
-                        date = getdate.get_date2()
-                    elif dateval == 3:
-                        date = getdate.get_datePA()
+
+                    if filename.startswith('BHO') or filename.startswith('SOM') or filename.startswith(
+                            'JUG') or filename.startswith('KAL'):
+                        date = GetDate.get_date(2, 1)
+                    elif filename.startswith('BD_'):
+                        date = GetDate.get_date(5, 2)
+                    elif filename.startswith('PROTHOM_ALO_A'):
+                        date = GetDate.get_date(2, 2)  # get_date3 can be used here
+                    elif filename.startswith('PROTHOM_ALO_C'):
+                        date = GetDate.get_date_bangla()
+
                     datedata = date.strftime('%Y/%m/%d')
                     output = datedata + '   ' + repr(output) + '    ' + filename + '\n'
                     # output = repr(output) + '   ' + datedata + '    ' + filename + '\n'
@@ -66,5 +58,5 @@ lines.sort()
 out.close()
 out = open('output data.txt', 'w', encoding='utf8')
 for line in lines:
-    out.write(repr(line) + '\n\n')
+    out.write(repr(line) + '\n')
 out.close()
